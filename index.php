f13b7b406710239d495258a9844dd5d3eff6b2f7
nicolasmpienda0918 <61071136+nicolasmpienda0918@users.noreply.github.com>
2020-02-21 21:11:47 -0500

    generalidadesPHP1

--- index.php
+++ index.php
@@ -1,8 +1,25 @@
 <?php
 
+//Ejercicio1 
 
-echo ("hola mundo");
+echo ("TALLER 1 GENERALIDADES PHP Elaboraado por Nicolas Meola <br><br>");
+echo ("Ejercicio 1 <br>");
+$numero1=25;
+$numero2=18;
 
+$suma=$numero1+$numero2;
+$resta=$numero1-$numero2;
+$multiplicacion=$numero1*$numero2;
+$divicion=$numero1/$numero2;
+
+echo("<br>La suma de:".$numero1."+".$numero2."=".$suma);
+echo("<br>La resta de:".$numero1."-".$numero2."=".$resta);
+echo("<br>La multiplicacion de:".$numero1."*".$numero2."=".$multiplicacion);
+echo("<br>La divicion de:".$numero1."/".$numero2."=".$divicion);
+
+echo ("<br><br>Ejercicio 2 <br>");
+$altura=1.78
+$peso=85
 
 
 
